71de371f84fc5c1cc86c70003e699fed8c95906c
신동아 <post1919@naver.com>
2021-08-13 19:56:34 +0900

    HelloResponseDto & 테스트 추가

---
+++ src/test/java/com/dongah/book/springboot/dto/HelloResponseDtoTest.java
@@ -0,0 +1,23 @@
+package com.dongah.book.springboot.dto;
+
+import com.dongah.book.springboot.web.dto.HelloResponseDto;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void 롬복테스트(){
+        //given
+        String name = "dongah";
+        int amount = 500;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        //then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
