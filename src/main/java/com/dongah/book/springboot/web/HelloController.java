90d318c3cbe74ed36b3386b91aff7afa705019a5
신동아 <post1919@naver.com>
2021-08-14 03:05:15 +0900

    dto 테스트코드

--- src/main/java/com/dongah/book/springboot/web/HelloController.java
+++ src/main/java/com/dongah/book/springboot/web/HelloController.java
@@ -1,12 +1,17 @@
 package com.dongah.book.springboot.web;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.dongah.book.springboot.web.dto.HelloResponseDto;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
     @GetMapping("/hello")
     public String hello(){
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount){
+        return new HelloResponseDto(name, amount);
     }
 }
