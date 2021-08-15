6af50feaf47b5a551a7e5dfddf3a4dc866f68576
신동아 <post1919@naver.com>
2021-08-15 17:30:13 +0900

    Posts INSERT / 테스트 추가

---
+++ src/main/java/com/dongah/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.dongah.book.springboot.service.posts;
+
+import com.dongah.book.springboot.domain.posts.PostsRepository;
+import com.dongah.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
