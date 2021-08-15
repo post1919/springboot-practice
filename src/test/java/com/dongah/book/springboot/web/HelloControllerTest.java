dfe3b4afd95088e624ddb2f94d6cb2ff3c2398e5
신동아 <post1919@naver.com>
2021-08-15 17:13:08 +0900

    HelloControllerTest 위치이동

--- src/test/java/com/dongah/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/dongah/book/springboot/web/HelloControllerTest.java
@@ -1,6 +1,5 @@
-package com.dongah.book.springboot;
+package com.dongah.book.springboot.web;
 
-import com.dongah.book.springboot.web.HelloController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
